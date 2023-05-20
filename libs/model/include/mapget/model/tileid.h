73b8bd63cd847e3c106ba6917e5befcf4add7d31
Joseph Birkner <j.birkner@klebert-engineering.de>
2023-05-20 15:26:58 +0200

    tileid: Added fromWgs84

--- libs/model/include/mapget/model/tileid.h
+++ libs/model/include/mapget/model/tileid.h
@@ -5,31 +5,64 @@
 namespace mapget
 {
 
-/// Structure to represent the Tile ID
+
+/**
+ * TileId class - represents a rectangular area on the globe, according to
+ * the following tiling scheme.
+ *
+ * Each tile is identified by a zoom level `z` and two grid
+ * coordinates `x` and `y`. *mapget* uses a binary tiling scheme for
+ * the earths surface: The zoom level `z` controls the number of subdivisions for
+ * the WGS84 longitudinal `[-180,180]` axis (columns) and latitudinal `[-90,90]` axis (rows).
+ * The tile `x` coordinate indicates the column, and the `y` coordinate indicates the row.
+ * On level zero, there are two columns and one row. In general, the number of rows is `2^z`,
+ * and the number of columns is `2^(z+1)`.
+ */
 struct TileId
 {
-    /// Constructor to initialize TileId with x, y, z
+    /**
+     * Constructor to initialize TileId with x, y, z
+     */
     TileId(uint16_t x, uint16_t y, uint16_t z);
 
-    /// Constructor to initialize TileId with value
+    /**
+     * Constructor to initialize TileId with value
+     */
     TileId(uint64_t const& value);  // NOLINT
 
-    /// Function to get x (column) component of the TileId
+    /**
+     * Function to create a TileId from WGS84 longitude, latitude, and zoom level.
+     */
+    static TileId fromWgs84(double longitude, double latitude, uint16_t zoomLevel);
+
+    /**
+     * Function to get x (column) component of the TileId
+     */
     [[nodiscard]] uint16_t x() const;
 
-    /// Function to get y (row) component of the TileId
+    /**
+     * Function to get y (row) component of the TileId
+     */
     [[nodiscard]] uint16_t y() const;
 
-    /// Function to get z (zoom level) component of the TileId (zoom level)
+    /**
+     * Function to get z (zoom level) component of the TileId (zoom level)
+     */
     [[nodiscard]] uint16_t z() const;
 
-    /// Operator overload for equality comparison
+    /**
+     * Operator overload for equality comparison
+     */
     bool operator==(TileId const&) const;
 
-    /// Operator overload for less than comparison
+    /**
+     * Operator overload for less than comparison
+     */
     bool operator<(TileId const&) const;
 
-    /// The value representing the TileId
+    /**
+     * The value representing the TileId, in 0x00xxyyzz format.
+     */
     uint64_t value_;
 };
 
--- libs/model/src/tileid.cpp
+++ libs/model/src/tileid.cpp
@@ -1,41 +1,61 @@
 #include "tileid.h"
+#include <cmath>
+#include <algorithm>
 
 namespace mapget
 {
 
-// Constructor to initialize TileId with x, y, z
 TileId::TileId(uint16_t x, uint16_t y, uint16_t z) {
     value_ = ((uint64_t)x << 32) | ((uint64_t)y << 16) | z;
 }
 
-// Constructor to initialize TileId with value
 TileId::TileId(uint64_t const& value) : value_(value) {
     // No extra work needed here as we directly assign the given value
 }
 
-// Function to get x component of the TileId
 uint16_t TileId::x() const {
     return (uint16_t) (value_ >> 32);
 }
 
-// Function to get y component of the TileId
 uint16_t TileId::y() const {
     return (uint16_t) ((value_ >> 16) & 0xFFFF);
 }
 
-// Function to get z component of the TileId (zoom level)
 uint16_t TileId::z() const {
     return (uint16_t) (value_ & 0xFFFF);
 }
 
-// Operator overload for equality comparison
 bool TileId::operator== (TileId const& other) const {
     return value_ == other.value_;
 }
 
-// Operator overload for less than comparison
 bool TileId::operator< (TileId const& other) const {
     return value_ < other.value_;
+}
+
+TileId TileId::fromWgs84(double longitude, double latitude, uint16_t zoomLevel)
+{
+    longitude = fmod(longitude, 360.) + 180.;
+    if (longitude < 0.)
+        longitude += 360.;
+
+    // Calculate the number of subdivisions
+    auto numCols = static_cast<uint16_t>(1 << (zoomLevel + 1));
+    auto numRows = static_cast<uint16_t>(1 << zoomLevel);
+
+    // Convert to grid coordinates
+    auto x = static_cast<int64_t>((longitude / 360.) * numCols);
+    auto y = static_cast<int64_t>(((90. - latitude) / 180.) * numRows);
+
+    x %= numCols;
+    y %= numRows * 2;
+    y = std::abs(y);  // -y: Crossing the North Pole
+
+    // We have crossed the South Pole
+    if (y >= numRows)
+        y = numRows - (y - numRows) - 1;
+
+    return {(uint16_t)x, (uint16_t)y, zoomLevel};
 }
 
 } // namespace mapget
