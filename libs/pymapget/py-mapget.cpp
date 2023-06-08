f64faca14688ff3c29bf0faccc9c31dd03a57623
Joseph Birkner <j.birkner@klebert-engineering.de>
2023-06-08 13:42:07 +0200

    pymapget: Added CMake/CI infrastructure.

---
+++ libs/pymapget/py-mapget.cpp
@@ -0,0 +1,14 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/stl_bind.h>
+#include <pybind11/stl.h>
+#include <pybind11/functional.h>
+#include <fstream>
+#include "stx/format.h"
+
+namespace py = pybind11;
+using namespace py::literals;
+using namespace std::string_literals;
+
+PYBIND11_MODULE(mapget, m)
+{
+}
