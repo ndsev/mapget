d398891721834f00a545c416ab72d628df1cbc13
Laura Lahesoo <l.lahesoo@klebert-engineering.de>
2023-08-02 15:21:58 +0200

    Use _putenv_s for tests with Windows.

--- test/test-file-logging.cpp
+++ test/test-file-logging.cpp
@@ -7,9 +7,15 @@ TEST_CASE("FileLogging", "[Logging]")
 {
     auto file_name = "logfile-test.log";
 
+#ifdef _WIN32
+    _putenv_s("MAPGET_LOG_LEVEL", "trace");
+    _putenv_s("MAPGET_LOG_FILE", file_name);
+    _putenv_s("MAPGET_LOG_FILE_MAXSIZE", "100000");
+#else
     setenv("MAPGET_LOG_LEVEL", "trace", 1);
     setenv("MAPGET_LOG_FILE", file_name, 1);
     setenv("MAPGET_LOG_FILE_MAXSIZE", "100000", 1);
+#endif
 
     std::filesystem::path test_log_file = std::filesystem::current_path() / file_name;
     std::cout << "Using test log file: " << test_log_file << std::endl;
