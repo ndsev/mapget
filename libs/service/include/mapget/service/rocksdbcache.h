25901f2e3dd8ec0fb83a3d26351bde5e259be5be
Laura Lahesoo <l.lahesoo@klebert-engineering.de>
2023-10-16 22:39:01 +0200

    rocksdb: Basic putTileLayer implementation.

--- libs/service/include/mapget/service/rocksdbcache.h
+++ libs/service/include/mapget/service/rocksdbcache.h
@@ -2,6 +2,8 @@
 
 #include "cache.h"
 
+#include <rocksdb/db.h>
+
 namespace mapget
 {
 
@@ -9,11 +11,18 @@ class RocksDBCache : public Cache
 {
 public:
     RocksDBCache();
+    ~RocksDBCache();
 
     std::optional<std::string> getTileLayer(MapTileKey const& k);
     void putTileLayer(MapTileKey const& k, std::string const& v);
     std::optional<std::string> getFields(std::string_view const& sourceNodeId);
     void putFields(std::string_view const& sourceNodeId, std::string const& v);
+
+private:
+    rocksdb::DB* db_;
+    rocksdb::Options options_;
+    rocksdb::WriteOptions write_options_;
+    rocksdb::ReadOptions read_options_;
 };
 
 }
--- libs/service/src/rocksdbcache.cpp
+++ libs/service/src/rocksdbcache.cpp
@@ -7,54 +7,39 @@ namespace mapget
 
 RocksDBCache::RocksDBCache()
 {
-    // TODO return the database instance.
-    // This is just a dummy test.
-
     auto db_path = "cache-rocksdb";
-
-    rocksdb::Options options;
-    options.create_if_missing = true;
-    options.WAL_ttl_seconds = 3600;  // Set TTL to 1 hour (adjust as needed).
+    options_.create_if_missing = true;
+    // Set TTL to 1 hour (adjust as needed).
+    options_.WAL_ttl_seconds = 3600;
 
     // Open the database.
-    rocksdb::DB* db;
-    rocksdb::Status status = rocksdb::DB::Open(options, db_path, &db);
+    rocksdb::Status status = rocksdb::DB::Open(options_, db_path, &db_);
     if (!status.ok()) {
         std::cerr << "Error opening database: " << status.ToString() << std::endl;
     }
+}
 
-    // Define a key and value.
-    std::string key = "dummy_key";
-    std::string value = "dummy_value";
-
-    // Put the key-value pair into RocksDB.
-    rocksdb::WriteOptions write_options;
-    status = db->Put(write_options, key, value);
-    if (!status.ok()) {
-        std::cerr << "Error writing to database: " << status.ToString() << std::endl;
-    }
-
-    // Read the value from RocksDB.
-    rocksdb::ReadOptions read_options;
-    std::string read_value;
-    status = db->Get(read_options, key, &read_value);
-    if (status.ok()) {
-        std::cout << "Key: " << key << " | Value: " << read_value << std::endl;
-    } else {
-        std::cerr << "Error reading from database: " << status.ToString() << std::endl;
-    }
-
-    // TODO destructor.
-    // Close the database.
-    delete db;
+RocksDBCache::~RocksDBCache() {
+    delete db_;
 }
 
 std::optional<std::string> RocksDBCache::getTileLayer(MapTileKey const& k) {
-
+    std::string read_value;
+    auto status = db_->Get(read_options_, k.toString(), &read_value);
+    if (status.ok()) {
+        std::cout << "Key: " << k.toString() << " | Value: " << read_value << std::endl;
+        return read_value;
+    } else {
+        std::cerr << "Error reading from database: " << status.ToString() << std::endl;
+        return {};
+    }
 }
 
 void RocksDBCache::putTileLayer(MapTileKey const& k, std::string const& v) {
-
+    auto status = db_->Put(write_options_, k.toString(), v);
+    if (!status.ok()) {
+        std::cerr << "Error writing to database: " << status.ToString() << std::endl;
+    }
 }
 
 std::optional<std::string> RocksDBCache::getFields(std::string_view const& sourceNodeId) {
