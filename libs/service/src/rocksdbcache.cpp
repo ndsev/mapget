0fa4f5e3954a2e4d26f915b8520240182f10552d
Laura Lahesoo <l.lahesoo@klebert-engineering.de>
2023-10-17 16:00:07 +0200

    cache: Use mapget log in RocksDB cache.

--- libs/service/src/rocksdbcache.cpp
+++ libs/service/src/rocksdbcache.cpp
@@ -1,6 +1,8 @@
-#include "rocksdbcache.h"
 #include <rocksdb/db.h>
 #include <iostream>
+
+#include "rocksdbcache.h"
+#include "mapget/log.h"
 
 namespace mapget
 {
@@ -15,7 +17,7 @@ RocksDBCache::RocksDBCache()
     // Open the database.
     rocksdb::Status status = rocksdb::DB::Open(options_, db_path, &db_);
     if (!status.ok()) {
-        std::cerr << "Error opening database: " << status.ToString() << std::endl;
+        logRuntimeError(stx::format("Error opening database: {}", status.ToString()));
     }
 }
 
@@ -27,18 +29,18 @@ std::optional<std::string> RocksDBCache::getTileLayer(MapTileKey const& k) {
     std::string read_value;
     auto status = db_->Get(read_options_, k.toString(), &read_value);
     if (status.ok()) {
-        std::cout << "Key: " << k.toString() << " | Value: " << read_value << std::endl;
+        log().trace(stx::format("Key: {} | Value: {}", k.toString(), read_value));
         return read_value;
-    } else {
-        std::cerr << "Error reading from database: " << status.ToString() << std::endl;
-        return {};
     }
+
+    logRuntimeError(stx::format("Error reading from database: {}", status.ToString()));
+    return {};
 }
 
 void RocksDBCache::putTileLayer(MapTileKey const& k, std::string const& v) {
     auto status = db_->Put(write_options_, k.toString(), v);
     if (!status.ok()) {
-        std::cerr << "Error writing to database: " << status.ToString() << std::endl;
+        logRuntimeError(stx::format("Error writing to database: {}", status.ToString()));
     }
 }
 
