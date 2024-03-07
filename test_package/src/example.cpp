ec9d5a15fc0efe710434f92f6e4071ade78b29cf
johannes-wolf <mail@johannes-wolf.com>
2024-03-07 18:55:32 +0100

    ci: Fix wheel deploy

--- test_package/src/example.cpp
+++ test_package/src/example.cpp
@@ -1,6 +1,11 @@
 #include "mapget/model/info.h"
 #include "mapget/service/service.h"
+#include "mapget/http-service/http-client.h"
+#include "mapget/http-datasource/datasource-client.h"
 
 int main() {
+    mapget::Version v;
+    (void)v.toJson();
+
     return 0;
 }
