4b4536a3fa465c9f5088083ed095b977df477154
Wagram Airiian <wagram.airiian@gmail.com>
2024-09-10 15:45:28 +0200

    Bump nlohmann json version

--- conanfile.py
+++ conanfile.py
@@ -51,7 +51,7 @@ class MapgetRecipe(ConanFile):
         self.requires("simfil/0.3.2", transitive_headers=True)
         self.requires("spdlog/[~1]", transitive_headers=True)
         self.requires("bitsery/[~5]")
-        self.requires("nlohmann_json/3.11.2", transitive_headers=True)
+        self.requires("nlohmann_json/3.11.3", transitive_headers=True)
         self.requires("glm/cci.20230113", transitive_headers=True)
         if self.options.with_httplib:
             self.requires("cli11/2.3.2")
