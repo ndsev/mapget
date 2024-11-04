73258a8f83bc4c02e16019035e317ce0d2fe5302
Joseph Birkner <j.birkner@klebert-engineering.de>
2024-11-04 15:39:36 +0100

    Bump simfil to v0.3.3.

--- deps.cmake
+++ deps.cmake
@@ -111,7 +111,7 @@ else()
     set(SIMFIL_SHARED          NO  CACHE BOOL "Simfil as static library")
     FetchContent_Declare(simfil
       GIT_REPOSITORY "https://github.com/Klebert-Engineering/simfil.git"
-      GIT_TAG        "string-pool-lookup-performance"
+      GIT_TAG        "v0.3.3"
       GIT_SHALLOW    ON)
     FetchContent_MakeAvailable(simfil)
   endif()
