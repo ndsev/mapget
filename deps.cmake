767f89c02bc59a9fabe25d3ff02856e987e66a65
Fabian Klebert <f.klebert@klebert-engineering.de>
2025-07-10 16:49:00 +0200

    Pin simfil dependency as latest main is broken

--- deps.cmake
+++ deps.cmake
@@ -168,8 +168,8 @@ if (NOT TARGET simfil)
     GIT_REPOSITORY "https://github.com/Klebert-Engineering/simfil.git"
     # TODO: We want to have the simfil diagnostics feature, there is
     #       not yet an official release containing it -> activate main branch
-    GIT_TAG        "main"
-    GIT_SHALLOW    ON)
+    GIT_TAG        "c280e45"
+    GIT_SHALLOW    OFF)
   FetchContent_MakeAvailable(simfil)
 endif()
 
