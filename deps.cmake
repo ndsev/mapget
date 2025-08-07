63c0bc782564c1224752511ba60ec55b05642b2f
Wagram Airiian <wagram.airiian@gmail.com>
2025-08-07 17:04:10 +0200

    Update simfil reference

--- deps.cmake
+++ deps.cmake
@@ -144,7 +144,7 @@ if (NOT TARGET simfil)
   set(SIMFIL_SHARED          NO  CACHE BOOL "Simfil as static library")
   FetchContent_Declare(simfil
     GIT_REPOSITORY "https://github.com/Klebert-Engineering/simfil.git"
-    GIT_TAG        "114-case-insensitive-enum-completion"
+    GIT_TAG        "v0.5.4"
     GIT_SHALLOW    ON)
   FetchContent_MakeAvailable(simfil)
 endif()
