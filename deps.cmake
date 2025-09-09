393b00b07288673ca5da661a7cea1ba195b06f31
Johannes Wolf <mail@johannes-wolf.com>
2025-09-09 09:47:35 +0200

    deps: Bump simfil to 0.5.5

--- deps.cmake
+++ deps.cmake
@@ -170,7 +170,7 @@ if (NOT TARGET simfil)
   set(SIMFIL_SHARED          NO  CACHE BOOL "Simfil as static library")
   FetchContent_Declare(simfil
     GIT_REPOSITORY "https://github.com/Klebert-Engineering/simfil.git"
-    GIT_TAG        "v0.5.4"
+    GIT_TAG        "v0.5.5"
     GIT_SHALLOW    ON)
   FetchContent_MakeAvailable(simfil)
 endif()
