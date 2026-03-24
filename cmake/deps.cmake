047faab5ec9de235d05612611dda7947f8703347
Joseph Birkner <j.birkner@klebert-engineering.de>
2026-03-24 19:54:15 +0100

    build: pick up python wheel test fix

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -161,7 +161,7 @@ if (MAPGET_WITH_SERVICE OR MAPGET_WITH_HTTPLIB OR MAPGET_ENABLE_TESTING)
 endif()
 
 if (MAPGET_WITH_WHEEL AND NOT TARGET python-cmake-wheel)
-    CPMAddPackage("gh:Klebert-Engineering/python-cmake-wheel#v1.2.2@1.2.2")
+    CPMAddPackage("gh:Klebert-Engineering/python-cmake-wheel#v1.2.3@1.2.3")
 endif()
 
 if (MAPGET_ENABLE_TESTING)
