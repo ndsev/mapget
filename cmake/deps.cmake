76b41dfbde31cc3358d8cdc26380ff5ce4ebcaaf
Joseph Birkner <j.birkner@klebert-engineering.de>
2026-05-05 16:16:48 +0200

    Bump simfil.

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -34,7 +34,7 @@ if (NOT "${_mapget_simfil_source_dir}" STREQUAL "")
             "SIMFIL_SHARED OFF")
 else()
     CPMAddPackage(
-        URI "gh:Klebert-Engineering/simfil#release/0.7.1"
+        URI "gh:Klebert-Engineering/simfil@0.7.1"
         OPTIONS
             "SIMFIL_WITH_MODEL_JSON ON"
             "SIMFIL_SHARED OFF")
